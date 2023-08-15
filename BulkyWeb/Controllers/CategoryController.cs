b0b9ad2742e49b9e3b18e7d4325209d320691425
Chris Petrevski <c_petrevski@hotmail.com>
2023-08-15 15:52:52 +1000

    Category Create view and controller update
    Category Create view and controller update

--- BulkyWeb/Controllers/CategoryController.cs
+++ BulkyWeb/Controllers/CategoryController.cs
@@ -17,5 +17,27 @@ namespace BulkyWeb.Controllers
 
             return View(objCategoryList);
         }
+
+        public IActionResult Create() 
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Category obj)
+        {
+            if (obj.Name == obj.DisplayOrder.ToString())
+            {
+                ModelState.AddModelError("name", "The Display Order cannot excatly match the Name");
+            }
+
+            if (ModelState.IsValid)
+            {
+                _db.Categories.Add(obj);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
     }
 }
