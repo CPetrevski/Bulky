cd10dc4edc7ecb762ef5797210f85733c3f372c7
Chris Petrevski <c_petrevski@hotmail.com>
2023-08-22 18:24:28 +1000

    Create Product Models and Views
    Create Product Models and Views
    Add Seed data for DB

--- Bulky.DataAccess/Data/ApplicationDbContext.cs
+++ Bulky.DataAccess/Data/ApplicationDbContext.cs
@@ -11,6 +11,7 @@ namespace Bulky.DataAccess.Data
         }
 
         public DbSet<Category> Categories { get; set; }
+        public DbSet<Product> Products { get; set; }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
@@ -18,6 +19,81 @@ namespace Bulky.DataAccess.Data
                 new Category { Id = 1, Name = "Action", DisplayOrder = 1 },
                 new Category { Id = 2, Name = "Science Fiction", DisplayOrder = 2 },
                 new Category { Id = 3, Name = "History", DisplayOrder = 3 }
+                );
+
+            modelBuilder.Entity<Product>().HasData(
+                new Product
+                {
+                    Id = 1,
+                    Title = "Fortune of Time",
+                    Author = "Billy Spark",
+                    Description = "Example Description for the selected book",
+                    ISBN = "PWSC111111101",
+                    ListPrice = 99,
+                    Price = 90,
+                    Price5 = 85,
+                    Price10 = 80
+                },
+                new Product
+                {
+                    Id = 2,
+                    Title = "Dark Skies",
+                    Author = "Nancy Hoover",
+                    Description = "Example Description for the selected book",
+                    ISBN = "LK55111111101",
+                    ListPrice = 40,
+                    Price = 30,
+                    Price5 = 25,
+                    Price10 = 20
+                },
+                new Product
+                {
+                    Id = 3,
+                    Title = "Vanish in the Sunset",
+                    Author = "Julian Button",
+                    Description = "Example Description for the selected book",
+                    ISBN = "ZZZZ1111111101",
+                    ListPrice = 55,
+                    Price = 50,
+                    Price5 = 40,
+                    Price10 = 35
+                },
+                new Product
+                {
+                    Id = 4,
+                    Title = "Cotton Candy",
+                    Author = "Abby Muscles",
+                    Description = "Example Description for the selected book",
+                    ISBN = "WSJW1111111101",
+                    ListPrice = 70,
+                    Price = 65,
+                    Price5 = 60,
+                    Price10 = 55
+                },
+                new Product
+                {
+                    Id = 5,
+                    Title = "Rock in the Ocean",
+                    Author = "Ron Parker",
+                    Description = "Example Description for the selected book",
+                    ISBN = "S0TJ1111111101",
+                    ListPrice = 30,
+                    Price = 27,
+                    Price5 = 25,
+                    Price10 = 20
+                },
+                new Product
+                {
+                    Id = 6,
+                    Title = "Leaves and Wonders",
+                    Author = "Laura Phanton",
+                    Description = "Example Description for the selected book",
+                    ISBN = "F0T01111111101",
+                    ListPrice = 25,
+                    Price = 23,
+                    Price5 = 22,
+                    Price10 = 20
+                }
                 );
         }
     }
---
+++ Bulky.DataAccess/Migrations/20230817055433_AddProductModelAndSeed.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace Bulky.DataAccess.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddProductModelAndSeed : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Products",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Title = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    ISBN = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Author = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    ListPrice = table.Column<double>(type: "float", nullable: false),
+                    Price = table.Column<double>(type: "float", nullable: false),
+                    Price5 = table.Column<double>(type: "float", nullable: false),
+                    Price10 = table.Column<double>(type: "float", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Products", x => x.Id);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Products",
+                columns: new[] { "Id", "Author", "Description", "ISBN", "ListPrice", "Price", "Price10", "Price5", "Title" },
+                values: new object[,]
+                {
+                    { 1, "Billy Spark", "Example Description for the selected book", "PWSC111111101", 99.0, 90.0, 80.0, 85.0, "Fortune of Time" },
+                    { 2, "Nancy Hoover", "Example Description for the selected book", "LK55111111101", 40.0, 30.0, 20.0, 25.0, "Dark Skies" },
+                    { 3, "Julian Button", "Example Description for the selected book", "ZZZZ1111111101", 55.0, 50.0, 35.0, 40.0, "Vanish in the Sunset" },
+                    { 4, "Abby Muscles", "Example Description for the selected book", "WSJW1111111101", 70.0, 65.0, 55.0, 60.0, "Cotton Candy" },
+                    { 5, "Ron Parker", "Example Description for the selected book", "S0TJ1111111101", 30.0, 27.0, 20.0, 25.0, "Rock in the Ocean" },
+                    { 6, "Laura Phanton", "Example Description for the selected book", "F0T01111111101", 25.0, 23.0, 20.0, 22.0, "Leaves and Wonders" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Products");
+        }
+    }
+}
