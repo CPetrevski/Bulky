b9748c6c197e7ed06757bead57bcfbae6817b83e
Chris Petrevski <c_petrevski@hotmail.com>
2023-09-20 17:52:12 +1000

    Manage Order Details
    Create Order model
    Create OrderController
    Create Order Index page
    Create order.js DataTable

---
+++ BulkyWeb/Areas/Admin/Controllers/OrderController.cs
@@ -0,0 +1,36 @@
+﻿using Bulky.DataAccess.Repository.IRepository;
+using Bulky.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BulkyWeb.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    [Authorize]
+    public class OrderController : Controller
+    {
+        private readonly IUnitOfWork _unitOfWork;
+
+        public OrderController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        #region API CALLS
+
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            List<OrderHeader> objOrderheaders = _unitOfWork.OrderHeader.GetAll(includeProperties: "ApplicationUser").ToList();
+            return Json(new { data = objOrderheaders });
+        }
+
+
+        #endregion
+    }
+}
---
+++ BulkyWeb/wwwroot/js/order.js
@@ -0,0 +1,29 @@
+﻿
+var dataTable;
+
+$(document).ready(function () {
+    loadDataTable();
+});
+
+function loadDataTable() {
+    dataTable = $('#tblData').DataTable({
+        "ajax": { url: '/admin/order/getall'},
+        "columns": [
+            { data: 'id', "width": "10%" },
+            { data: 'name', "width": "20%" },
+            { data: 'phoneNumber', "width": "10%" },
+            { data: 'applicationUser.email', "width": "20%" },
+            { data: 'orderStatus', "width": "20%" },
+            { data: 'orderTotal', "width": "20%" },
+            {
+                data: 'id',
+                "render": function (data) {
+                    return `<div class="w-75 btn-group" role="group">
+                    <a href="/admin/order/details?orderId=${data}" class="btn btn-primary mx-2"> <i class="bi bi-pencil-square"></i></a>
+                    </div>`
+                },
+                "width": "25%"
+            }
+        ]
+    });
+}
